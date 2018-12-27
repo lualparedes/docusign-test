7e1b1282ca975e598ef0e6454c1252c5bcbd8e90
Luis Paredes <lualps94@gmail.com>
2018-12-27 19:31:42 -0400

    Added guard for serving according to the type of environment

--- src/server.js
+++ src/server.js
@@ -1,18 +1,20 @@
-/**
- * @note {1} Comment out these lines when building for production.
- */
-
 import express from 'express';
-import webpack from 'webpack'; // {1}
-import webpackMiddleware from 'webpack-dev-middleware'; // {1}
-import webpackConfig from '../webpack.config.js'; // {1}
+import docusign from 'docusign-esign';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.js';
+
+
+const apiClient = new docusign.ApiClient();
 
 const app = express();
-app.use(webpackMiddleware(webpack(webpackConfig))); // {1}
+if (process.env.NODE_ENV === 'development') {
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+}
 
 app.get('/', (req, res) => {
-  res.send('Hola bebé, yo se que contigo no sirve la labia 😉')
+  res.send('Holamundo')
 })
 app.listen(4000, () => {
-  console.log('Listening');
+  console.log(`Listening in ${process.env.NODE_ENV} environment`);
 });
